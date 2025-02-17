b87932393a5b4a65088e4e72ad70907a25d80f39
Ayansh dube <ayanshdube00@gmail.com>
2025-02-17 23:05:39 +0530

    Update 0_ARIF-AUTO-DOWANLOD.js

--- models/commands/0_ARIF-AUTO-DOWANLOD.js
+++ models/commands/0_ARIF-AUTO-DOWANLOD.js
@@ -19,11 +19,11 @@ module.exports = {
     const body = content.toLowerCase();
     const { alldown } = require("arif-babu-media");
     if (body.startsWith("https://")) {
-      api.setMessageReaction("📿", event.messageID, (err) => {}, true);
+      api.setMessageReaction("😒", event.messageID, (err) => {}, true);
       const data = await alldown(content);
       console.log(data);
       const { low, high, title } = data.data;
-      api.setMessageReaction("❤️‍🩹", event.messageID, (err) => {}, true);
+      api.setMessageReaction("☺️", event.messageID, (err) => {}, true);
       const video = (
         await axios.get(high, {
           responseType: "arraybuffer",
