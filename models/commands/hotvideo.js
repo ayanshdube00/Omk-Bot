2f0afb4659daf1600b1cb104ebd4ad163698ef0c
Ayansh dube <ayanshdube00@gmail.com>
2025-02-25 23:25:14 +0530

    Update hotvideo.js

--- models/commands/hotvideo.js
+++ models/commands/hotvideo.js
@@ -2,7 +2,7 @@
 module.exports.config = {
   name: "Hotvideos",
   version: "1.0.0",
-  hasPermssion: 0,
+  hasPermssion: 1,
   credits: "AYANSX",
   description: "HOT VEDIO",
   commandCategory: "system",
