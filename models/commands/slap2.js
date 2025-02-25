15b7829c445c50855ee01ba108ab623a72a62334
Ayansh dube <ayanshdube00@gmail.com>
2025-02-25 23:20:42 +0530

    Update slap2.js

--- models/commands/slap2.js
+++ models/commands/slap2.js
@@ -1,44 +1,30 @@
 module.exports.config = {
-  name: "slap2",
-  version: "1.0.0",
-  hasPermssion: 1,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "Slap the friend tag",
-  commandCategory: "general",
-  usages: "slap [Tag someone you want to slap]",
-  cooldowns: 5,
+        name: "slap2",
+        version: "7.3.1",
+        hasPermssion: 1,
+        credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+        description: "spank",
+        commandCategory: "edit-img",
+        usages: "[blank or tag]",
+        cooldowns: 5,
+        dependencies: {"fs-extra": "","discord.js": "","discord-image-generation" :"","node-superfetch": ""}
 };
 
+module.exports.run = async ({ event, api, args, Users }) => {
+  const DIG = global.nodemodule["discord-image-generation"];
+  const Discord = global.nodemodule['discord.js'];
+  const request = global.nodemodule["node-superfetch"];
+  const fs = global.nodemodule["fs-extra"];
+   let { senderID, threadID, messageID } = event;
+  var id = Object.keys(event.mentions)[0] || event.senderID;
+  var ids = Object.keys(event.mentions)[1] || event.senderID;
 
-module.exports.run = async ({ api, event, args }) => {
-	const axios = require('axios');
-	const request = require('request');
-	const fs = require("fs");
-    var out = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
-  if (!args.join("")) return out("Please tag someone");
-  else
-  return axios.get('https://api.waifu.pics/sfw/slap').then(res => {
-        let getURL = res.data.url;
-        let ext = getURL.substring(getURL.lastIndexOf(".") + 1);
-        var mention = Object.keys(event.mentions)[0];
-                  let tag = event.mentions[mention].replace("@", "");    
-        
- let callback = function () {
-            api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-        api.sendMessage({
-						        body: "Slapped! " + tag + "\n\n*sorry, i thought there's mosquito*",
-                                          mentions: [{
-          tag: tag,
-          id: Object.keys(event.mentions)[0]
-        }],
-						attachment: fs.createReadStream(__dirname + `/cache/slap.${ext}`)
-					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/slap.${ext}`), event.messageID)
-				};
- //   }
-        request(getURL).pipe(fs.createWriteStream(__dirname + `/cache/slap.${ext}`)).on("close", callback);
-			})
-    .catch(err => {
-                     api.sendMessage("Failed to generate gif, be sure that you've tag someone!", event.threadID, event.messageID);
-    api.setMessageReaction("☹️", event.messageID, (err) => {}, true);
-                  })     
+  var avatar = (await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).body;
+  var avatars = (await request.get(`https://graph.facebook.com/${ids}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).body;
+
+  let img = await new DIG.Spank().getImage(avatars, avatar);
+  let attach = new Discord.MessageAttachment(img);
+  var path_trash = __dirname + "/cache/spank.png";
+  fs.writeFileSync(path_trash, attach.attachment);
+  api.sendMessage({attachment: fs.createReadStream(path_trash)}, event.threadID, () => fs.unlinkSync(path_trash), event.messageID);
 }
