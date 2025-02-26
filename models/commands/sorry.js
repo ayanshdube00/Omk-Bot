bc3a1f469ef143189b8155627d085a59263c0658
Ayansh dube <ayanshdube00@gmail.com>
2025-02-26 13:15:01 +0530

    Update sorry.js

--- models/commands/sorry.js
+++ models/commands/sorry.js
@@ -1,26 +1,23 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "sorry",
-    version: "1.1.1",
+  name: "Sorry",
+    version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝑱𝑼𝑳𝑴𝑰 𝑱𝑨𝑨𝑻", 
-  description: "Just Respond",
+  credits: "VanHung - Fixed by LTD", 
+  description: "hihihihi",
   commandCategory: "no prefix",
+  usages: "🙂",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  let react = event.body.toLowerCase();
-  if(react.includes("Me ayansh hu BSDK") ||
-     react.includes("Tera malik hu") || react.includes("Tera boss hu sale") || react.includes("Tera papa hu") ||
-react.includes("Tera boss hu ayansh") ||
-react.includes("Malik se bckchodi")) {
+  if (event.body.indexOf("betaa")==0 || event.body.indexOf("meri billi mujhe hi meow")==0 || event.body.indexOf("Apne Malik se bakchodi")==0 || event.body.indexOf("bataun Abhi")==0) {
     var msg = {
-        body: `【  𝐒0𝐑𝐑𝐘 𝐀𝐁 𝐆𝐀𝐋𝐓𝐈 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑𝐔𝐍𝐆𝐀 𝐏𝐋𝐒 𝐀𝐊 𝐁𝐀𝐑 𝐌𝐀𝐅 𝐊𝐀𝐑 𝐃0______🤐😙😥】`,
+        body: "𝐒0𝐑𝐑𝐘 𝐀𝐁 𝐆𝐀𝐋𝐓𝐈 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑𝐔𝐍𝐆𝐀 𝐏𝐋𝐒 𝐀𝐊 𝐁𝐀𝐑 𝐌𝐀𝐅 𝐊𝐀𝐑 𝐃0_🤐😙😥",
       }
-      api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("👍", event.messageID, (err) => {}, true)
+      api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😢", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
