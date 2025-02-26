afa13a9ebf0e20f0dab0176bbcd39e43d8d20b91
Ayansh dube <ayanshdube00@gmail.com>
2025-02-26 15:51:26 +0530

    Update 0_ARIF-PAIR-4.js

--- models/commands/0_ARIF-PAIR-4.js
+++ models/commands/0_ARIF-PAIR-4.js
@@ -77,5 +77,5 @@ module.exports. run = async function({ api, event, args, Users, Threads, Currenc
         var sex = await data[id].gender;
         var gender = sex == 2 ? "Male🧑" : sex == 1 ? "Female👩‍  " : "Tran Duc Bo";
 var one = senderID, two = id;
-    return makeImage({ one, two }).then(path => api.sendMessage({ body: `${namee}🫶🏻${name} \n Brekp hone ki Smbhavna:👉🏻『${tle}』`, mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    return makeImage({ one, two }).then(path => api.sendMessage({ body: `${namee}__🫶🏻__${name} \n  👇🏻𝐛𝐫𝐞𝐤𝐮𝐩 𝐡𝐨𝐧𝐞 𝐤𝐚 𝐜𝐡𝐚𝐧𝐱:-『${tle}』😱😱😱`, mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
 }
