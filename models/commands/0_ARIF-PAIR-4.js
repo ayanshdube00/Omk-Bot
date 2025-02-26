46a6d1394fd7fcc6d2a1db843c24e34b6c2ae921
Ayansh dube <ayanshdube00@gmail.com>
2025-02-26 10:27:02 +0530

    Update 0_ARIF-PAIR-4.js

--- models/commands/0_ARIF-PAIR-4.js
+++ models/commands/0_ARIF-PAIR-4.js
@@ -1,73 +1,81 @@
-const PREM_BABU= "PREM-PROJECT"; // Original credit
-
 module.exports.config = {
   name: "pair4",
-  version: "1.0.0", 
+  version: "1.0.1",
   hasPermssion: 0,
-  credits: PREM_BABU,
-  description: "THIS BOT WAS MADE BY MR PREM BABU",
-  commandCategory: "LOVE PAIRING",
-  usePrefix: false,
-  usages: "PREFIX", 
-  cooldowns: 5
-};
+  credits: "",
+  description: "",
+  usePrefix: true,
+  commandCategory: "Picture",
+  cooldowns: 5,
+  dependencies: {
+        "axios": "",
+        "fs-extra": ""
+    }
+}
+module.exports.onLoad = async() => {
+    const { resolve } = global.nodemodule["path"];
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { downloadFile } = global.utils;
+    const dirMaterial = __dirname + `/cache/canvas/`;
+    const path = resolve(__dirname, 'cache/canvas', 'aar2.jpg');
+    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/JZX0BI1.jpg", path);
+}
+async function makeImage({ one, two }) {
+    const fs = global.nodemodule["fs-extra"];
+    const path = global.nodemodule["path"];
+    const axios = global.nodemodule["axios"]; 
+    const jimp = global.nodemodule["jimp"];
+    const __root = path.resolve(__dirname, "cache", "canvas");
 
-module.exports.run = async function({ api, event, Threads, Users }) {
-  const axios = global.nodemodule["axios"];
-  const fs = global.nodemodule["fs-extra"];
+    let pairing_img = await jimp.read(__root + "/aar2.jpg");
+    let pathImg = __root + `/pairing_${one}_${two}.png`;
+    let avatarOne = __root + `/avt_${one}.png`;
+    let avatarTwo = __root + `/avt_${two}.png`;
 
-  var { participantIDs } = (await Threads.getData(event.threadID)).threadInfo;
-  var tle = Math.floor(Math.random() * 101);
-  var namee = (await Users.getData(event.senderID)).name
-  const botID = api.getCurrentUserID();
-  const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
-  var id = listUserID[Math.floor(Math.random() * listUserID.length)];
-  var name = (await Users.getData(id)).name
-  var arraytag = [];
-  arraytag.push({id: event.senderID, tag: namee});
-  arraytag.push({id: id, tag: name});
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
 
-  // Check if credit has been modified
-  if (module.exports.config.credits !== PREM_BABU) {
-    return api.sendMessage("DON'T CHANGE CREDIT! PREM-PROJECT FUCK YOUR MOTHER AND SISTER", event.threadID, event.messageID);
-  }
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
 
-  // Array of Imgur gif links
-  const imgurGifs = [
-    "https://i.imgur.com/41ogamD.gif",
-    "https://i.imgur.com/qVpA7RB.gif",
-    "https://i.imgur.com/OJ6uIXY.gif",
-    "https://i.imgur.com/3qSoi8K.gif",
-    "https://i.imgur.com/EO5EtJp.gif",
-    "https://i.imgur.com/fP5kc0h.gif"
-    // Add more Imgur gif links as needed
-  ];
+    let circleOne = await jimp.read(await circle(avatarOne));
+    let circleTwo = await jimp.read(await circle(avatarTwo));
+    pairing_img.composite(circleOne.resize(350, 350), 700, 180).composite(circleTwo.resize(365, 360), 160, 181);
 
-  // Select a random Imgur gif link
-  const randomImgurLink = imgurGifs[Math.floor(Math.random() * imgurGifs.length)];
+    let raw = await pairing_img.getBufferAsync("image/png");
 
-  // Download the gif from the selected Imgur link
-  const gifData = (await axios.get(randomImgurLink, { responseType: "arraybuffer" })).data; 
-  fs.writeFileSync(__dirname + "/cache/giflove.png", Buffer.from(gifData, "utf-8"));
+    fs.writeFileSync(pathImg, raw);
+    fs.unlinkSync(avatarOne);
+    fs.unlinkSync(avatarTwo);
 
-  // Download avatars
-  let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
-  fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
+    return pathImg;
+}
+async function circle(image) {
+    const jimp = require("jimp");
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+}
+module.exports. run = async function({ api, event, args, Users, Threads, Currencies }) {
+  const axios = require("axios");
+    const fs = require("fs-extra");
+    const { threadID, messageID, senderID } = event;
+    var tl = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
+        var tle = tl[Math.floor(Math.random() * tl.length)];
+        let dataa = await api.getUserInfo(event.senderID);
+        let namee = await dataa[event.senderID].name
+        let loz = await api.getThreadInfo(event.threadID);
+        var emoji = loz.participantIDs;
+        var id = emoji[Math.floor(Math.random() * emoji.length)];
+        let data = await api.getUserInfo(id);
+        let name = await data[id].name
+        var arraytag = [];
+                arraytag.push({id: event.senderID, tag: namee});
+                arraytag.push({id: id, tag: name});
 
-  let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
-  fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
-
-  // Prepare attachments
-  var imglove = [];
-  imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-  imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-  imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
-
-  var msg = {
-    body: `LO MIL GAYA AAP KA JIVAN SATHI 🙂🖐️\nAB BAR BAR MAT BOLNA SETTING KARVANE KI 😒👈\nAAP DONO KA PYAR 👉 ${tle}% है\n`+namee+" "+"💖"+" "+name, 
-    mentions: arraytag, 
-    attachment: imglove
-  };
-
-  return api.sendMessage(msg, event.threadID, event.messageID);
+        var sex = await data[id].gender;
+        var gender = sex == 2 ? "Male🧑" : sex == 1 ? "Female👩‍  " : "Tran Duc Bo";
+var one = senderID, two = id;
+    return makeImage({ one, two }).then(path => api.sendMessage({ body: `🩷🩷${namee}${name} \n 『${tle}』`, mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
 }
