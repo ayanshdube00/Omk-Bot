ab1e01608bcf390273f3bc16a9fef83c04d90d51
Ayansh dube <ayanshdube00@gmail.com>
2025-02-28 23:44:29 +0530

    Update Emoge2.js

--- models/commands/Emoge2.js
+++ models/commands/Emoge2.js
@@ -23,7 +23,7 @@ react.includes("😹")) {
                                 body: ``,
                         }
                         api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🥺", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😹", event.messageID, (err) => {}, true)
                 }
         }
         module.exports.run = function({ api, event, client, __GLOBAL }) {
