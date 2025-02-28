38729b2b9da738fd01b26d178d7606786e5ddd3a
Ayansh dube <ayanshdube00@gmail.com>
2025-02-28 22:04:53 +0530

    Update 0_ARIF-MENTION.js

--- models/commands/0_ARIF-MENTION.js
+++ models/commands/0_ARIF-MENTION.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = function({ api, event }) {
     var aid = ["61554958589328","100094941194624", "100080287361043","100094941194624" , "100009293679350"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["oye Mere se bat Kar use mention ni😏", "Kya Hua mere owner ko q Bula Rhe Ho meri sikayt to ni kroge 😶", "Wo Shayad Busy h😐🙄", "usko mention ni kro 🤛🏻", "Kya Kam Hai mujhe batao na 😌", "ufff Kya pareshani he tmhari muje batao use ni bulao🙃", "Kya kaam hai mujhse bolo🏋🏻", "sab pata chal raha hai 🙂" , "ky hua mujhe batao mai unko bta dunga 😶"];
+      var msg = ["Mujse bolo use mention ni😏", "Kya Hua muje btao😶", "Wo Shayad Busy h😐🙄", "usko mention ni kro 🤛🏻", "Kya Kam Hai mujhe batao na 😌", "ufff Kya pareshani he🤦🏻", "Kya kaam hai mujhse bolo🏋🏻", "sab pata chal raha hai 🙂" , "ky hua mujhe batao mai unko bta dunga 😶"];
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
     }}
