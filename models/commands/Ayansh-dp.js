312fc49f0856db49180ca268186a241308a41563
Ayansh dube <ayanshdube00@gmail.com>
2025-02-28 22:14:43 +0530

    Update and rename gm.js to Ayansh-dp.js

--- models/commands/Ayansh-dp.js
+++ models/commands/Ayansh-dp.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 module.exports.config = {
-  name: "gm",
+  name: "Aynsdp",
   version: "1.0.1",
   hasPermssion: 0,
   credits: "Ayanshx", 
@@ -11,11 +11,11 @@ module.exports.config = {
   cooldowns: 5, 
 };
 
-const imgurLink = "https://i.ibb.co/gMHsxrmM/481846140-1375982787166741-5863945781615120926-n-jpg-stp-dst-jpg-p480x480-tt6-nc-cat-102-ccb-1-7-nc.jpg" ,""https://i.ibb.co/wNbWdZQ0/481731626-1305310810504120-7051884851648639123-n-jpg-stp-dst-jpg-p480x480-tt6-nc-cat-105-ccb-1-7-nc.jpg";
+const imgurLink = ""https://i.ibb.co/JRVhgry1/474218768-1289672395592887-783903429045807969-n-jpg-stp-dst-jpg-s640x640-tt6-nc-cat-108-ccb-1-7-nc-s.jpg"," ,""https://i.ibb.co/wNbWdZQ0/481731626-1305310810504120-7051884851648639123-n-jpg-stp-dst-jpg-p480x480-tt6-nc-cat-105-ccb-1-7-nc.jpg";
 
 module.exports.handleEvent = async function({ api, event }) {
   var { threadID, messageID } = event;
-  if (event.body.startsWith("gud morning") || event.body.startsWith("Good Morning") || event.body.startsWith("good morning") || event.body.startsWith("suprabhat")) {
+  if (event.body.startsWith("@Ayansh") || event.body.startsWith("ayansh") || event.body.startsWith("@ayansh") || event.body.startsWith("Ayansh")) {
     try {
       const response = await axios.get(imgurLink, { responseType: "stream" });
       var msg = {
@@ -23,7 +23,7 @@ module.exports.handleEvent = async function({ api, event }) {
         attachment: response.data
       };
       api.sendMessage(msg, threadID, messageID);
-      api.setMessageReaction("🩷", messageID, (err) => {}, true);
+      api.setMessageReaction("😎", messageID, (err) => {}, true);
     } catch (error) {
       console.error("Failed to fetch image:", error.message);
     }
