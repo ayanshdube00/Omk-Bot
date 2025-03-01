b07111707aceba12dba6e806fa95ea57d7d7cf8e
Ayansh dube <ayanshdube00@gmail.com>
2025-03-01 17:52:05 +0530

    Update Ayansh-dp.js

--- models/commands/Ayansh-dp.js
+++ models/commands/Ayansh-dp.js
@@ -11,7 +11,7 @@ module.exports.config = {
   cooldowns: 5, 
 };
 
-const imgurLink = "https://i.ibb.co/JRVhgry1/474218768-1289672395592887-783903429045807969-n-jpg-stp-dst-jpg-s640x640-tt6-nc-cat-108-ccb-1-7-nc-s.jpg";
+const imgurLink = "https://i.ibb.co/7d9g3J4y/481698911-1200816758043661-170311540644864906-n-jpg-stp-dst-jpg-s640x640-tt6-nc-cat-105-ccb-1-7-nc-s.jpg";
 
 module.exports.handleEvent = async function({ api, event }) {
   var { threadID, messageID } = event;
