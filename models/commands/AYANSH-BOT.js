8ff34b26cf249c4218c1781b7adaebe5f2db02ca
Ayansh dube <ayanshdube00@gmail.com>
2025-03-01 07:46:23 +0530

    Update AYANSH-BOT.js

--- models/commands/AYANSH-BOT.js
+++ models/commands/AYANSH-BOT.js
@@ -94,7 +94,7 @@ if ((event.body.toLowerCase() == "Sun") || (event.body.toLowerCase() == "suno"))
    };
    
   if ((event.body.toLowerCase() == "") || (event.body.toLowerCase() == "") || (event.body.toLowerCase() == "") || (event.body.toLowerCase() == "")) {
-     return api.sendMessage("🤦🏻‍♀️", threadID);
+     return api.sendMessage("🤜🏻", threadID);
    };
    
   if ((event.body.toLowerCase() == "ludo khele") || (event.body.toLowerCase() == "ludo link do") || (event.body.toLowerCase() == "ludo") || (event.body.toLowerCase() == "ludo link")) {
