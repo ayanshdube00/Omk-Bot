2731bf77a57d7039b28e301bb26fc276ad471242
Ayansh dube <ayanshdube00@gmail.com>
2025-03-05 10:45:15 +0530

    Create help3.js

---
+++ models/commands/help3.js
@@ -0,0 +1,44 @@
+module.exports.config = {
+    name: "help3",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "SHANKAR-SUMAN",
+    description: "beginners guide",
+    usages: "[all/-a] [number of pages]",
+    commandCategory: "guides",
+    cooldowns: 5
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    const { commands } = global.client;
+    const { threadID } = event;
+    const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+    const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
+
+    const commandGroups = new Map();
+    const imgP = [];
+
+    for (const command of commands.values()) {
+        const category = command.config.commandCategory.toUpperCase();
+        if (!commandGroups.has(category)) {
+            commandGroups.set(category, []);
+        }
+        commandGroups.get(category).push(`✪${command.config.name}`);
+    }
+
+    let message = "";
+    for (const [category, commands] of commandGroups.entries()) {
+        message += `╭────────────✦\n│  ${category}\n├───✦\n`;
+        message += `${commands.join(" │")}\n`;
+        message += "├───✦\n";
+        message += "╰───────────⧕\n\n";
+    }
+
+    message += `╭────────────✦\n│ »      ☆𝐴𝑎𝑟𝑦𝑎𝑛 𝐵𝑜𝑡☆ 
+    《${commands.size} commands》 
+    𝓬𝓻𝓮𝓭𝓲𝓽 ● 𝓜𝓻.☆𝓐𝓪𝔂𝓪𝓷×\n│ » 《SAMRAT PROJECT》\n𝓯𝓮𝓮𝓵 𝓽𝓱𝓮 𝓹𝓸𝔀𝓮𝓻 𝓸𝓯 𝓜𝓻.\n╰────────────✦`;
+
+    return api.sendMessage(message, threadID, (error, info) => {
+        if (error) console.log(error);
+    });
+}
